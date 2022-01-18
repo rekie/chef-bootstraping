aec9c9543e40e652c4d3c95451e82137b743f30f
Reinhard Kiesswetter <reinhard@scerus.com>
2022-01-18 16:32:27 +0100

    initial commit win bootsrapping

---
+++ win/chef-boostrap.ps1
@@ -0,0 +1,52 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $ChefClientVersion,
+    # Parameter help description
+    [Parameter()]
+    [string]
+    $ValidatorKey,
+    # Parameter help description
+    [Parameter()]
+    [string]
+    $PolicyGroup,
+    # Parameter help description
+    [Parameter()]
+    [string]
+    $PolicyName,
+    # Parameter help description
+    [Parameter()]
+    [string]
+    $ChefServerURL,
+    # Parameter help description
+    [Parameter()]
+    [string]
+    $ChefValidationClientName
+)
+
+### Install chef client
+. { Invoke-WebRequest -useb https://omnitruck.chef.io/install.ps1 } | Invoke-Expression; install -version $ChefClientVersion
+
+$nodename = $env:computerName
+
+$validator = $ValidatorKey
+$pathValidationKey = "C:\chef\" + $ChefValidationClientName + ".pem"
+
+Set-Content -Path C:\chef\scerus-validator.pem -Value $validator
+
+$clientrb = @"
+chef_license            'accept'
+log_location            STDOUT
+chef_server_url         "$ChefServerURL"
+validation_client_name  "$ChefValidationClientName"
+validation_key          "$pathValidationKey"
+node_name               "$nodename"
+policy_group            "$policygroup"
+policy_name             "$policyname"
+"@
+
+Set-Content -Path c:\chef\client.rb -Value $clientrb
+
+### Run Chef-client
+c:\opscode\chef\bin\chef-client.bat
